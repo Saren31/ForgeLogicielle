553d44d5a9fa87ee9bf86b22b5fe31546532a047
Lucas Veslin <lucas.thibaud@hotmail.com>
2025-04-29 15:01:10 +0200

    Création d'une classe portefeuille temporaire pour faire les autres US [US4]:  Création du portefeuille #12

---
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -0,0 +1,63 @@
+package fr.utc.miage.shares;
+
+public class Portefeuille {
+    
+    private List<Action> actions;
+    private double solde;
+
+    public Portefeuille() {
+        this.actions = new ArrayList<>();
+        this.solde = 0.0;
+    }
+
+    public void ajouterAction(Action action) {
+        this.actions.add(action);
+    }
+
+    public void retirerAction(Action action) {
+        this.actions.remove(action);
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    public double valeurTotale() {
+        return 0.0; 
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Portefeuille :\n");
+        sb.append("Solde : ").append(solde).append("\n");
+        sb.append("Actions :\n");
+        for (Action action : actions) {
+            sb.append(action.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public int equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Portefeuille that = (Portefeuille) o;
+        return Double.compare(that.solde, solde) == 0 && Objects.equals(actions, that.actions);
+    }
+
+    public int hashCode() {
+        return Objects.hash(actions, solde);
+    }
+
+}
