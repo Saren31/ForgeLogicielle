bcf01cd5243fbab0213f80e1f8e7839555eb5302
Lucas Veslin <lucas.thibaud@hotmail.com>
2025-04-29 15:06:21 +0200

    Création de la Classe Portefeuille temporaire pour les autres US
    Lucas

---
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -0,0 +1,55 @@
+package fr.utc.miage.shares;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portefeuille {
+    
+    private List<Action> actions;
+    private double solde;
+
+    public Portefeuille() {
+        this.actions = new ArrayList<>();
+        this.solde = 0.0;
+    }
+
+    public void ajouterAction(Action action) {
+        this.actions.add(action);
+    }
+
+    public void retirerAction(Action action) {
+        this.actions.remove(action);
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    public double valeurTotale() {
+        return 0.0; 
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Portefeuille :\n");
+        sb.append("Solde : ").append(solde).append("\n");
+        sb.append("Actions :\n");
+        for (Action action : actions) {
+            sb.append(action.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+}
